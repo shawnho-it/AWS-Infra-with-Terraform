814d400f8254fe0764808689cfcc72e497179fdf
Shawn <84618646+shawnho-it@users.noreply.github.com>
2022-10-13 17:56:09 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "private_instances_names" {
   type    = list(any)
   default = ["terraform-appserver", "terraform-webserver", "terraform-dbserver"]
 }
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
